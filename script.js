c1479287988be248bc251a769553d3ff167a04a1
flores-97 <floresvictor15@gmail.com>
2025-11-26 16:26:09 -0800

    created function to update display as user clicks numbers. also what numbers have been selected and prevent numbers like '01' to be used

--- script.js
+++ script.js
@@ -7,5 +7,37 @@ document.addEventListener('DOMContentLoaded', () => {
     const numberButtons = document.querySelectorAll('[id^="button-"][id$="0"], [id^="button-"][id$="1"], [id^="button-"][id$="2"], [id^="button-"][id$="3"], [id^="button-"][id$="4"], [id^="button-"][id$="5"], [id^="button-"][id$="6"], [id^="button-"][id$="7"], [id^="button-"][id$="8"], [id^="button-"][id$="9"]');//this line will look for 'id' in html with the word button and the numbers next to them.
     const operatorButtons = document.querySelectorAll('#button-plus, #button-minus, #button-multiply, #button-divide');//will look for the the mathematical signs.
 
+    let currentExpression = '';//will store math equation as selected by user. 
+    let isEvaluated = false;//will check if whether '=' has been pressed.
+
+    //local storage items
+    const savelastExpression = localStorage.getItem('lastExpression');
+    const saveLastResult = localStorage.getItem('lastResult');
+
+    if (savelastExpression && saveLastResult) {
+        lastResultDisplay.textContent = '${saveLastExpression} = ${saveLastResult}';
+    }//will show the last expression entered by user.
+    
+    function updateDisplay(value) {
+        display.textContent = value;
+    }//shows users input into 'display' area at top of calculator 
+
+    function handleNumClick(event) {
+        if (isEvaluated) return;
+        const value = event.target.textContent;//will look for users clicks on numbers 
+
+        const lastIsOperator = /[+\-*/]/.test(currentExpression.slice(-1));
+        const currentOperatorIsEmpty = lastIsOperator || currentExpression === '';//registers numbers clicks
+
+        if (currentOperatorIsEmpty && value === '0'){
+            if (!currentExpression.endswith('0') || lastIsOperator) {
+                currentExpression += value;//check whether last entry was number or operator 
+            }
+            else {
+                currentExpression += value;
+            }
+            updateDisplay(currentExpression);//should prevent user from using number like '01'
+        }
+    }
 });
     
